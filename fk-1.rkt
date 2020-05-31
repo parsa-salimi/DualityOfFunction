11900a38f15ecd801ce57b0d9a4765d47dba7f6e
parsa-salimi <parsa2000kurd@gmail.com>
2020-05-31 17:46:50 -0400

    rewrote frequency, rudimentary tree generator

--- fk-1.rkt
+++ fk-1.rkt
@@ -83,16 +83,27 @@
 ;a better variable than one that merely satisfies the FK frequency criterea
 (define (frequent f g)
   ;clause is the clause of minimum length, f is the other formula, for now just return the first element
-  (define (frequent-help clause f) (car clause))
+  (define (frequent-help clause f)
+    (define (iterate func list accum lastfrequency)
+      (cond [(empty? list) accum]
+            [else (let ((frequency (func (first list))))
+                    (if (> frequency lastfrequency) (iterate func (rest list) (first list) frequency)
+                        (iterate func (rest list) accum lastfrequency)))]))
+    (define (frequency var formula)
+      (length (filter (lambda (x) (member var x)) formula)))
+    (iterate (lambda (x) (frequency x f)) (rest clause) (first clause) 0))
   (let ((min-f (minimum-clause f ))
         (min-g (minimum-clause g )))
     (if (< (length min-f) (length min-g)) (frequent-help min-f g) (frequent-help min-g f))))
 
 
-  
-(define (FK f g)
-  (define (FK-irr f g)
-    (printf "f:~a g:~a\n" f g)
+;tree=0 : left
+;tree=1 : right
+(define (FK f g tree)
+    (begin
+     (cond [(= tree 0) (printf "left:\t f:~a g:~a\n" f g)]
+           [(= tree 1) (printf "\t\t right:\t f:~a g:~a\n" f g)]
+           [else (printf "\troot:\t f:~a g:~a\n" f g)])
     (cond [ (not (sanitycheck f g)) false (printf "sanity check failed")]
           [ (<= (* (clause-len f) (clause-len g)) 1) (easydual f g)]
           [ else (letrec ((x (frequent f g))
@@ -100,8 +111,9 @@
                           (f1 (remove-clause f x))
                           (g0 (remove-var g x))
                           (g1 (remove-clause g x)))
-                 (and (FK f1 (disjunction g0 g1)) (FK g1 (disjunction f0 f1))))]))
-  (FK-irr (reduce f) (reduce g)))
+                 (and (FK (reduce f1) (reduce (disjunction g0 g1)) 0) (FK (reduce g1) (reduce (disjunction f0 f1)) 1)))])))
+(define (fk-run f g) (FK f g 2))
+  
 
 ;hardcoded for now(for testing) will autogenerate later
 (define f-1 '((1) (2)))
